b2cf9ab8e4de80b4f9ffdbc19d0927157b349527
kabishan <ksuvendran@gmail.com>
2024-12-16 18:09:55 -0500

    Update version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,7 +91,7 @@ android {
         applicationId = "com.kabishan.dairyfreedining"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
     }
     packaging {
